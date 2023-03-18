874f7df30ef05f81c384aeedffb9e7bd19c9a454
zs12341 <rewin6455@gmail.com>
2023-03-18 15:04:20 +0900

    feat : ApiException

---
+++ src/main/java/com/example/footballwebproject/game/GameResponseDto.java
@@ -0,0 +1,25 @@
+package com.example.footballwebproject.game;
+
+import com.example.footballwebproject.entity.Game;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+public class GameResponseDto {
+    private Long id;
+    private String teamA;
+    private String teamB;
+    private int scoreA;
+    private int scoreB;
+
+    public GameResponseDto(Game game){
+        this.id = game.getId();
+        this.teamA = game.getTeamA();
+        this.teamB = game.getTeamB();
+        this.scoreA = game.getScoreA();
+        this.scoreB = game.getScoreB();
+    }
+
+
+}
