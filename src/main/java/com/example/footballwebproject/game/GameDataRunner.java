6fce3704e44e62986d6cbcf5a4afd76e320bb73a
zs12341 <rewin6455@gmail.com>
2023-03-22 11:11:32 +0900

    feat : Game 자동생성

---
+++ src/main/java/com/example/footballwebproject/game/GameDataRunner.java
@@ -0,0 +1,29 @@
+package com.example.footballwebproject.game;
+
+import com.example.footballwebproject.entity.Game;
+import com.example.footballwebproject.exception.ApiException;
+import com.example.footballwebproject.exception.ExceptionEnum;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class GameDataRunner implements ApplicationRunner {
+    private final GameService gameService;
+
+    @Override
+    public void run(ApplicationArguments args) {
+        GameRequestDto requestDto = new GameRequestDto();
+        requestDto.setTeamA("Real Madrid");
+        requestDto.setTeamB("Team Korea");
+        requestDto.setScoreA(0);
+        requestDto.setScoreB(17);
+        requestDto.setImageA("https://secure.cache.images.core.optasports.com/soccer/teams/75x75/uuid_3kq9cckrnlogidldtdie2fkbl.png");
+        requestDto.setImageB("https://secure.cache.images.core.optasports.com/soccer/teams/75x75/uuid_1yghbv1c71b37eenutbwnvvq.png");
+
+        gameService.createGame(requestDto);
+    }
+
+}
--- src/main/java/com/example/footballwebproject/game/GameRequestDto.java
+++ src/main/java/com/example/footballwebproject/game/GameRequestDto.java
@@ -11,4 +11,32 @@ public class GameRequestDto {
     private int scoreB;
     private String imageA;
     private String imageB;
+
+    public void setGameId(Long gameId) {
+        this.gameId = gameId;
+    }
+
+    public void setTeamA(String teamA) {
+        this.teamA = teamA;
+    }
+
+    public void setTeamB(String teamB) {
+        this.teamB = teamB;
+    }
+
+    public void setScoreA(int scoreA) {
+        this.scoreA = scoreA;
+    }
+
+    public void setScoreB(int scoreB) {
+        this.scoreB = scoreB;
+    }
+
+    public void setImageA(String imageA) {
+        this.imageA = imageA;
+    }
+
+    public void setImageB(String imageB) {
+        this.imageB = imageB;
+    }
 }
