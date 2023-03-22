4c3270a7aeedb4233b9931681febd8a1fc21ebf1
narustudio <slackjawed12@gmail.com>
2023-03-22 21:16:23 +0900

    fix : ci

--- deploy.sh
+++ deploy.sh
@@ -13,7 +13,7 @@ if [ -z "$CURRENT_PID" ]; then
   echo "현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
   echo "> kill -15 $CURRENT_PID" >> deploy.log
-  kill -15 $CURRENT_PID
+  kill -SIGTERM $CURRENT_PID
   sleep 5
 fi
 
