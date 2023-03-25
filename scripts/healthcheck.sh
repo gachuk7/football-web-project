5876cdd6276a39b4954c1967405a78456073a576
narustudio <slackjawed12@gmail.com>
2023-03-25 19:15:15 +0900

    NGINX 실험 마지막

---
+++ scripts/healthcheck.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+# CURRENT_PORT : 현재 구동중인 애플리케이션의 포트
+CURRENT_PORT=$(cat /home/ubuntu/service_url.inc | grep -Po '[0-9]+' | tail -1)
+# TARGET_PORT : 변경할 포트
+TARGET_PORT=0
+
+if [ $CURRENT_PORT -eq 8081 ]; then
+  TARGET_PORT=8082
+elif [ $CURRENT_PORT -eq 8082 ]; then
+  TARGET_PORT=8081
+else
+  echo "> No WAS is connected to nginx"
+  exit 1
+fi
+
+# 새로 띄운 서버 헬스체크
+echo "> Health check of WAS at 'http://127.0.0.1:${TARGET_PORT}' ..."
+
+for RETRY_COUNT in $(seq 10);
+do
+    echo "> #${RETRY_COUNT} trying..."
+    RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}"  http://127.0.0.1:${TARGET_PORT}/health)
+
+    if [ ${RESPONSE_CODE} -eq 200 ]; then
+        echo "> New WAS successfully running"
+        exit 0
+    elif [ ${RETRY_COUNT} -eq 10 ]; then
+        echo "> Health check failed."
+        exit 1
+    fi
+    sleep 10
+done
--- scripts/switch.sh
+++ scripts/switch.sh
@@ -1,32 +1,26 @@
 #!/usr/bin/env bash
 
 PROJECT_ROOT="/home/ubuntu/app"
-JAR_FILE="$PROJECT_ROOT/spring-webapp.jar"
 
 DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
-
-TIME_NOW=$(date +%c)
 
 # CURRENT_PORT : 현재 구동중인 애플리케이션의 포트
 CURRENT_PORT=$(cat /home/ubuntu/service_url.inc | grep -Po '[0-9]+' | tail -1)
 # TARGET_PORT : 변경할 포트
 TARGET_PORT=0
 
-# 포트번호 변경
 if [ $CURRENT_PORT -eq 8081 ]; then
   TARGET_PORT=8082
 elif [ $CURRENT_PORT -eq 8082 ]; then
   TARGET_PORT=8081
 else
   echo "> No WAS is connected to nginx"
+  exit 1
 fi
 
-# TARGET_PORT 구동중인 애플리케이션 pid
-CURRENT_PID=$(lsof -Fp -i TCP:${TARGET_PORT} | grep -Po 'p[0-9]+' | grep -Po '[0-9]+')
+# 프록시 포트번호 변경
+echo "set \$service_url http://127.0.0.1:${TARGET_PORT};" | tee /home/ubuntu/service_url.inc
+echo "> Now Nginx proxies to ${TARGET_PORT}." >> $DEPLOY_LOG
 
-# TARGET_PORT 구동중이면 kill
-if [ ! -z $CURRENT_PID ]; then
-  echo "$TIME_NOW > 실행중인 $CURRENT_PID 애플리케이션 종료 " >> $DEPLOY_LOG
-  kill -15 $CURRENT_PID
-fi
-
+# nginx reload
+sudo service nginx -s reload
